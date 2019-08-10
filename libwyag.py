73b23cdbd4a495c61577107df311b321e3ac747d
tMinami <otherthan.new@gmail.com>
2019-08-10 14:38:21 +0900

    update

--- libwyag.py
+++ libwyag.py
@@ -15,6 +15,24 @@ argparser = argparse.ArgumentParser(description="The stupid content tracker")
 argsubparsers = argparser.add_subparsers(title="Commands", dest="command")
 argsubparsers.required = True
 
+argsp = argsubparsers.add_parser('rev-parse', help='Parse revision (or other objects) identifiers')
+argsp.add_argument('--wyag-type',
+                   metavar='type', dest='type',
+                   choices=['blob', 'commit', 'tag', 'tree'],
+                   default=None, help='Specify the expected type')
+argsp.add_argument('name',
+                   help='The name to parse')
+
+
+def cmd_rev_parse(args):
+    if args.type:
+        fmt = args.type.encode()
+
+    repo = repo_find()
+
+    print(object_find(repo, args.name, args.type, follow=True))
+
+
 argsp = argsubparsers.add_parser('tag', help='List and create tags')
 argsp.add_argument('-a', action='store_true', dest='create_tag_object', help='Whether to create a tag object')
 argsp.add_argument('name', nargs='?', help='The new tag\'s name')
@@ -611,6 +629,33 @@ def object_write(obj, actually_write=True):
 
 
 def object_find(repo, name, fmt=None, follow=True):
+    sha = object_resolve(repo, name)
+    if not sha:
+        raise Exception('No such reference {0}'.format(name))
+    if len(sha) > 1:
+        raise Exception('Ambiguous reference {0}: Candidates are:\n - {1}.'.format(name, '\n - '.join(sha)))
+
+    sha = sha[0]
+    if not fmt:
+        return sha
+
+    while True:
+        obj = object_read(repo, sha)
+
+        if obj.fmt == fmt:
+            return sha
+
+        if not follow:
+            return None
+
+        # Follow tags
+        if obj.fmt == b'tag':
+            sha = obj.kvlm[b'object'].decode('ascii')
+        elif obj.fmt == b'commit' and fmt == b'tree':
+            sha = obj.kvlm[b'tree'].decode('ascii')
+        else:
+            return None
+
     return name
 
 
@@ -647,3 +692,61 @@ def object_read(repo, sha):
 
         # Call constructor and retur object
         return c(repo, raw[y+1:])
+
+
+def object_resolve(repo, name):
+    """Resolve name to an object hash in repo.
+    This function is aware of:
+        - the HEAD literal
+        - short and long hases
+        - tags
+        - branches
+        - remote branches"""
+
+    candidates = list()
+    hashRE = re.compile(r'^[0-9A-Fa-f]{1,16}$')
+    # smallHashRE = re.compile(r'^[0-9A-Fa-f]{1,16}')
+
+    # Empty string? Abort.
+    if not name.strip():
+        return None
+
+    # Head is nonambiguous
+    if name == 'HEAD':
+        return [ref_resolve(repo, 'HEAD')]
+
+    if hashRE.match(name):
+        if len(name) == 40:
+            # This is a complete hash
+            return [name.lower()]
+        elif len(name) >= 4:
+            # This is a small hash 4 seems to be the minimal length
+            # for git to consider something a short hash.
+            # This limit is documented in man git-rev-parse
+            name = name.lower()
+            prefix = name[0:2]
+            path = repo_dir(repo, 'objects', prefix, mkdir=False)
+            if path:
+                rem = name[2:]
+                for f in os.listdir(path):
+                    if f.startwith(rem):
+                        candidates.append(prefix + f)
+    return candidates
+
+
+class GitIndexEntry(object):
+    ctime = None
+    mtime = None
+    dev = None
+    ino = None
+    mode_type = None
+    mode_perms = None
+    uid = None
+    gid = None
+    size = None
+    obj = None
+    flag_assume_valid = None
+    flag_extended = None
+    flag_stage = None
+    flag_name_length = None
+    name = None
